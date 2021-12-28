0c9305e37a255b08594dc6f7daaf72ca066b2641
vaibhav <vaibhav.jain1@tesco.com>
2021-12-28 14:50:44 +0530

    homepage into smaller components and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import Directory from '../../components/directory/directory.component';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homePage'>
+    <Directory />
+  </div>
+)
+
+export default HomePage;
