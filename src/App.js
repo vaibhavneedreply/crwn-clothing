2986a9c8b195e17d6929c43f0f472b95e1cdbac2
vaibhav <vaibhav.jain1@tesco.com>
2021-12-29 09:40:20 +0530

    update shop page with collection and collection item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shoppage.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/hats' element={<HatsPage />} />
+        <Route path='/shop' element={<ShopPage />} />
       </Routes>
     </div>
   );
--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,28 +1,7 @@
 import './menu-item.styles.scss';
 import { useNavigate } from 'react-router-dom';
 
-// const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-//   <div
-//     className={`${size} menu-item`} 
-//     onClick={() => history.push(`${match.url}${linkUrl}`)}>
-//     <div
-//       className='background-image'
-//       style={{
-//         backgroundImage: `url(${imageUrl})`
-//       }}
-//     />
-//     <div className='content'>
-//       <h1 className='title'>{title.toUpperCase()}</h1>
-//       <span className='subtitle'>SHOP NOW</span>
-//     </div>
-//   </div>
-// );
-
-// export default withRouter(MenuItem);
-
-
-
-export default (({ title, imageUrl, size, history, linkUrl, match }) => {
+export default (({ title, imageUrl, size, linkUrl }) => {
   const navigate = useNavigate();
   return (
     <div
