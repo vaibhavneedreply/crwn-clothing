7bf17006090962cde4958576385eb3b17f85c2af
vaibhav <vaibhav.jain1@tesco.com>
2021-12-29 14:36:51 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sing-in-and-sign-up/sing-in-and-sign-up.components';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/sing-in-and-sign-up/sing-in-and-sign-up.components.jsx
@@ -0,0 +1,8 @@
+import './sing-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+  <SignIn className='sing-in-and-sign-up'>SIGN IN</SignIn>
+)
+
+export default SignInAndSignUpPage;
