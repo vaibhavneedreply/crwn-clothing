9b9da836fa73f861a1188750842b6b3ef0f3e42b
vaibhav <vaibhav.jain1@tesco.com>
2021-12-29 17:19:50 +0530

    signup

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
   constructor(props) {
@@ -14,10 +14,15 @@ class SignIn extends Component {
     }
   }
 
-  handleSummit = event => {
+  handleSummit = async event => {
     event.preventDefault();
-
-    this.setState({email: '', password: ''})
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '', password: ''})
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   handleChange = event => {
